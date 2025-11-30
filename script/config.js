7797e9c6f7ab692c32316c6c7df9c17fe6490530
OGear <qaugier@guardiaschool.fr>
2025-11-30 22:55:06 +0100

    weeknd work

--- script/config.js
+++
@@ -1,3 +0,0 @@
-// listes pour le jeu
-const listeMots = ['Cachalot', 'Pétunia', 'Serviette']
-const listePhrases = ['Pas de panique', 'Phrase courte', 'Bien le bonjour']
--- script/script.js
+++ script/script.js
@@ -1,42 +1,7 @@
+const toggleButton = document.getElementById('toggleBtn');
+const sidebar = document.getElementById('sidebar');
 
-
-
-function afficherResultat(score, nbMotsProposes) {
-    console.log('Score : ' + score + ' / ' + nbMotsProposes)
+function toggleSidebar(){
+    sidebar.classList.toggle('close');
+    toggleButton.classList.toggle('rotate');
 }
-
-function choisirPhrasesOuMots() {
-    let choix = prompt("mots ou phrases ?")
-    while (choix !== "mots" && choix !== "phrases") {
-        choix = prompt("mots ou phrases ?")
-    }
-    return choix
-}
-
-function LancerBoucleDeJeu(listePropositions) {
-    let score = 0
-    for (let i = 0; i < listePropositions.length; i++) {
-        let motUtilisateur = prompt('Entrez le mot : ' + listePropositions[i])
-        if (motUtilisateur === listePropositions[i]) {
-            score++
-        }
-    }
-    return score
-}
-
-function lancerJeu() {
-    let choix = choisirPhrasesOuMots()
-    let score = 0
-    let nbMotsProposes = 0
-
-    if (choix === "mots" ) {
-        score = LancerBoucleDeJeu(listeMots)
-        nbMotsProposes = listeMots.length
-    } else {
-        score = LancerBoucleDeJeu(listePhrases)
-        nbMotsProposes = listePhrases.length
-    }
-
-    afficherResultat(score, nbMotsProposes)
-}
-
